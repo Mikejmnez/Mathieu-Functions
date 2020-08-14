574355dd1cbcb2f559d4913d7f59b4cd45df3714
Miguel Jimenez-Urias <mjimen17@jh.edu>
2020-08-14 16:19:56 -0400

    fixed typo

--- Mathieu_Functions/functions.py
+++ Mathieu_Functions/functions.py
@@ -20,8 +20,25 @@ def matrix_system(q, N, type='even', period='one'):
     if type is 'even' and period is 'one':
         d = [(2. * r) ** 2 for r in range(N)]
         e = q * _np.ones(N - 1)
-        A = _np.diag(d) + _np.diag(e, k=-1), + _np.diag(e, k=1)
+        A = _np.diag(d) + _np.diag(e, k=-1) + _np.diag(e, k=1)
         A[0, 1] = _np.sqrt(2) * A[0, 1]
         A[1, 0] = A[0, 1]
 
     return A
+
+
+def eig_pairs(A, type='real'):
+    ''' Calculates the characteristic value (eigenvalue) and the Fourier
+    coefficients associated with the Mathieu function. Both the eigenvalues
+    and Fourier coefficients are given in ascending order.
+    '''
+    N = len(A[:, 0])  # size of square matrix.
+    w, V = _LA.eig(A)  # calculates the eigenvalue
+    V[0, :] = V[0, :] / np.sqrt(2)
+    Fcoeff = []
+    for n in range(N):
+        Fcoeff.append(abs(V[0, n]) / _np.sqrt(2))
+
+    return w, Fcoeff
+
+
